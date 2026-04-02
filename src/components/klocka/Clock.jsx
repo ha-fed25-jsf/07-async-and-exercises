a6c522e01fdab2bac19224ad799d150ce83e368a
David Andersson <lejonmanen@gmail.com>
2026-04-02 14:32:37 +0200

    setTimeout

---
+++ src/components/klocka/Clock.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const Clock = () => {
+	const [message, setMessage] = useState('Ingen tid har gått...')
+	const [enabled, setEnabled] = useState(true)
+
+	const handleClick = () => {
+		console.log('handleClick före')
+		// Obs! set-funktionerna körs inte direkt
+		setMessage('... tiden går...')
+		setEnabled(false)
+
+		setTimeout(() => {
+			console.log('handleClick efter')
+			setMessage('Nu är det dags! Sekunden är klar!')
+			setEnabled(true)
+		}, 2000)
+		// använd clearTimeout eller clearInterval för att avbryta timeouts och intervall
+	}
+
+	return (
+		<>
+			<button onClick={handleClick} disabled={!enabled}> Vänta 1 sekund </button>
+			<p> {message} </p>
+		</>
+	)
+}
+
+export default Clock
